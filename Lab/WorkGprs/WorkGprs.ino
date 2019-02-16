ea03f05a4de3a54b80e3fc2ccb86ef89164043fe
Joao Mota Jr <joaomotajr@hotmail.com>
2019-02-15 23:22:02 -0200

    fix (gprs) some issues

--- Lab/WorkGprs/WorkGprs.ino
+++ Lab/WorkGprs/WorkGprs.ino
@@ -2,13 +2,19 @@
 #include <LGPRS.h>
 #include <LGPRSClient.h>
 
-const char * apn = "claro.com.br";
-const char * user = "claro";
-const char * pass = "claro";
+const char* apn = "claro.com.br";
+const char* user = "claro";
+const char* pass = "claro";
 
-char host[] = "arduino.cc";
-char uri[] = "/asciilogo.txt";
-int port = 80;
+char host[] = "177.144.134.145";
+char uri[] = "/api/historic/SaveByPositionUid2/144/166934";
+int port = 8090;
+
+int connGPRS(char host[], int port);
+int getHTTP(char host[], char uri[]);
+void attachGPRS(const char* apn, const char* user, const char* pass);
+
+int result = 0;
 
 LGPRSClient client;
 
@@ -19,7 +25,7 @@ void setup()
     while (!Serial) continue;
 
     attachGPRS(apn, user, pass);
-    connGPRS(host, port);
+    result = connGPRS(host, port);
     
 }
 
@@ -35,8 +41,10 @@ void loop()
         Serial.println("Erro, aguardando nova conexão!");
         delay(30000);
         attachGPRS(apn, user, pass);
-        connGPRS(host, port);
+        result = connGPRS(host, port);
     }
 
     Serial.println("Fim Ciclo.");
 }
+
+
