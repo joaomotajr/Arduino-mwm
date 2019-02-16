c4f0f85493053b47f5b9e73d54831c1867904947
Joao Mota Jr <joaomotajr@hotmail.com>
2019-02-15 23:22:02 -0200

    fix (gprs) some issues

--- Lab/WorkGprs/functions.ino
+++ Lab/WorkGprs/functions.ino
@@ -1,4 +1,5 @@
-void attachGPRS(char apn, char user, char pass) {
+
+void attachGPRS(const char* apn, const char* user, const char* pass) {
 
   Serial.println("Acessando Rede GRPS");
   while (!LGPRS.attachGPRS(apn, user, pass))
@@ -9,79 +10,101 @@ void attachGPRS(char apn, char user, char pass) {
   Serial.println("Attached");
 }
 
-bool connGPRS(char host, char port) {
+int connGPRS(char host[], int port) {
 
   client.setTimeout(60000);
   if (client.connect(host, port))
   {
     Serial.println("connected");
-    return true;
+    return 1;
   }
   else
   {  
     Serial.println("connection failed");
-    return false;
+    return 0;
   }
 }
 
-bool getHTTP(char server, char uri) {
-// string getHTTP(char server, char uri, int id, float value) {
-
-    Serial.println("Getting Data...");
+int getHTTP(char* host, char* uri) {
+    Serial.println("Getting Data... From :: " + String(host) + String(uri));
 
     // Make a HTTP request:
-    client.print("GET " + uri);    
-    // client.print("GET ");    
-    // client.print(uri +  String(id) +  "/"+ String(value ));
+    client.print("GET ");    
+    client.print(uri);
     client.println(" HTTP/1.1");
-    client.print("Host: " + server);
+    client.print("Host: ");
+    client.println(host);
     client.println("Connection: close");
     
     if (client.println() == 0) {
         Serial.println("Failed to send request");
-        return false;
+        client.stop();
+        return 0;
     }
 
     Serial.println("Receiving data...");
+    readClient();
 
-    // Check HTTP status
-    char status[32] = {0};
-    client.readBytesUntil('\r', status, sizeof(status));
-    if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
-        Serial.print("Unexpected response: ");
-        Serial.println(status);
-        return false;
-    }
-
-    // Skip HTTP headers
-    char endOfHeaders[] = "\r\n\r\n";
-    if (!client.find(endOfHeaders)) {
-        Serial.println("Invalid response");
-        return false;
-    }
-
-    // Allocate JsonBuffer
-    // Use arduinojson.org/assistant to compute the capacity.
-    const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
-    DynamicJsonBuffer jsonBuffer(capacity);
-
-    // Parse JSON object
-    JsonObject& root = jsonBuffer.parseObject(client);
-    if (!root.success()) {
-        Serial.println("Parsing failed!");
-        return false;
-    }
-
-    // Extract values
-    Serial.println(F("Response:"));
-    Serial.println(root["sensor"].as<char*>());
-    Serial.println(root["time"].as<char*>());
-    Serial.println(root["data"][0].as<char*>());
-    Serial.println(root["data"][1].as<char*>());
+//    // Check HTTP status
+//    char status[32] = {0};
+//    client.readBytesUntil('\r', status, sizeof(status));
+//    if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+//        Serial.print("Unexpected response: ");
+//        Serial.println(status);
+//        client.stop();
+//        return 0;
+//    }
+//
+//    // Skip HTTP headers
+//    char endOfHeaders[] = "\r\n\r\n";
+//    if (!client.find(endOfHeaders)) {
+//        Serial.println("Invalid response");
+//        client.stop();
+//        return 0;
+//    }
+//
+//    // Allocate JsonBuffer
+//    // Use arduinojson.org/assistant to compute the capacity.
+//    const size_t capacity = 0 + 10;
+//    DynamicJsonBuffer jsonBuffer(capacity);   
+//
+//    // Parse JSON object
+//    JsonObject& root = jsonBuffer.parseObject(client);
+//    if (!root.success()) {
+//        Serial.println("Parsing failed!");
+//        client.stop();
+//        return 0;
+//    }
+//
+//    // Extract values
+//    Serial.println(F("Response:"));
+//    Serial.println(root["sensor"].as<char*>());
+//    Serial.println(root["time"].as<char*>());
+//    Serial.println(root["data"][0].as<char*>());
+//    Serial.println(root["data"][1].as<char*>());
 
     // Disconnect
     client.stop();
 
-    return true;
+    return 1;
+}
 
+void readClient() {
+  if (client.available())
+  {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.available() && !client.connected())
+  {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    for (;;)
+      ;
+  }
 }
