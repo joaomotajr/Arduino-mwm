7ed16cdb8ee603f681ce7bf4a0adf751c73ed6c0
Joao Mota Jr <joaomotajr@hotmail.com>
2019-04-03 23:09:39 -0300

    feat (datalogger2) add

---
+++ Lab/Datalogger2/Datalogger2.ino
@@ -0,0 +1,66 @@
+#include <ArduinoJson.h>
+#include <Wire.h>
+#include <stdio.h>
+#include "Nanoshield_LCD.h"
+//#include "Nanoshield_RTC.h"
+#include "RTClib.h"
+
+char idDevice[] = "156";
+float highValue = 12;
+float lowValue = 0;
+int logLevel = 3;   // 3 : Cartão, Envio e Check Response | 2 : Cartão e Envio | 1 : Cartão  | 0 : Só Leitura
+int delayTime = 5000;
+
+//Nanoshield_RTC rtc;
+RTC_DS1307 rtc;
+Nanoshield_LCD lcd;
+
+void setup() {
+    Serial.begin(9600);
+    
+    pinMode(13, OUTPUT);
+    warningLight(2);
+
+    lcd.begin();
+    lcd.clear();
+    lcd.print("Inicializando...");
+
+    initRTC();
+    initSD();
+    initADC();
+    inicializa();
+    lcd.clear();
+}
+
+void loop() {
+  
+  float flow = processADC();     
+  delay(delayTime);     
+}
+
+void inicializa() {
+  Serial.println("---------------------------------------------");  
+  Serial.println("Nenhuma Transmissão Configurada"); 
+  initFeedback();
+}
+
+void initFeedback() { 
+  Serial.println("---------------------------------------------");
+  Serial.println("Iniciando Processamento...");
+  Serial.println("---------------------------------------------");
+  Serial.println("");
+  warningLight(2);  
+}
+
+bool initRTC() {
+  Serial.println("---------------------------------------------");
+  Serial.print("Módulo RTC sensor: ");
+
+  if (!rtc.begin()) {
+    Serial.println("[Falha]");
+    return false;
+  };
+  rtc.now();
+  Serial.println("[Iniciado]");
+  return true;
+}
